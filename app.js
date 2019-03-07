30f10f5294f54097cd517ed64bb218ca63fa0a72
tommy-carlier <tommy@tcx.be>
2019-03-07 12:00:16 +0100

    Further debugging

--- app.js
+++ app.js
@@ -146,7 +146,7 @@ async function cancelEdit() {
 
 async function saveEdit(type) {
   const newDate = new Date(editOccurrenceDate.value + ' ' + editOccurrenceTime.value);
-  alert(newDate.toString());
+  alert('"' + editOccurrenceDate.value + ' ' + editOccurrenceTime.value + '" -> ' + newDate.toString());
   await data.modifyOccurrence(type, editingDate, newDate);
   await displayDayOccurrences(newDate);
   showScreen('dayView');
