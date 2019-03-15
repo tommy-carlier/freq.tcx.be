638cdfc9bdaac4bff5ca5f9b3717d598c072e6e3
tommy-carlier <tommy@tcx.be>
2019-03-15 14:39:02 +0100

    Make width of bars relative to maxCount

--- js/statsView.js
+++ js/statsView.js
@@ -5,7 +5,7 @@ import ui from './ui.js';
 const statsView = ui.fromID('statsView'),
       last30DaysTable = ui.fromID('statsLast30Days');
 
-function appendDayRow(f, label, value, rowClass) {
+function appendDayRow(f, label, value, maxValue, rowClass) {
   const row = document.createElement('TR');
   if(rowClass) row.classList.add(rowClass);
 
@@ -13,13 +13,13 @@ function appendDayRow(f, label, value, rowClass) {
   row.appendChild(document.createElement('TD')).textContent = value;
 
   const barCell = row.appendChild(document.createElement('TD'));
-  barCell.appendChild(document.createElement('DIV')).style.width = value + 'em';
+  barCell.appendChild(document.createElement('DIV')).style.width = (100 * value / maxValue) + '%';
 
   f.appendChild(row);
 }
 
-function appendDateRow(f, date, count) {
-  appendDayRow(f, time.formatDateList(date), count, date.getDay() == 1 ? 'BorderTop' : null);
+function appendDateRow(f, date, count, maxCount) {
+  appendDayRow(f, time.formatDateList(date), count, maxCount, date.getDay() == 1 ? 'BorderTop' : null);
 }
 
 async function loadLast30DaysTable(target) {
@@ -30,13 +30,13 @@ async function loadLast30DaysTable(target) {
   var statistics = new Statistics();
   await statistics.loadOccurrences(target, time.addDays(today, -30), today);
   for(var [date, count] of statistics.dayCounts) {
-    appendDateRow(f, date, count);
+    appendDateRow(f, date, count, statistics.maxCount);
   }
 
   if(statistics.totalDays > 0) {
-    appendDayRow(f, 'Minimum', statistics.minCount, 'BorderTop');
-    appendDayRow(f, 'Average', Math.round(statistics.averageCount));
-    appendDayRow(f, 'Maximum', statistics.maxCount);
+    appendDayRow(f, 'Minimum', statistics.minCount, statistics.maxCount, 'BorderTop');
+    appendDayRow(f, 'Average', Math.round(statistics.averageCount), statistics.maxCount);
+    appendDayRow(f, 'Maximum', statistics.maxCount, statistics.maxCount);
   }
 
   last30DaysTable.appendChild(f);
